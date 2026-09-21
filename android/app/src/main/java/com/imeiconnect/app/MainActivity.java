07c9b03474bae0eec2e23d586a58c2423520fcda
Manuel0612-code <mavundaemmanuel373@gmail.com>
2026-09-21 11:34:03 -0700

    Add Capacitor Android app and APK build

---
+++ android/app/src/main/java/com/imeiconnect/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.imeiconnect.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.imeiconnect.app',
+  appName: 'IMEI Connect',
+  webDir: 'out',
+  android: {
+    backgroundColor: '#f4f7f5',
+  },
+};
+
+export default config;
